5b555805420541c096a69afe9e1abf5087ac9f54
OMPRAKASH MISHRA <omprakashmishra3978@gmail.com>
2025-07-15 10:57:46 -0700

    feat: flyway common module and readme updates

---
+++ terraform/modules/flyway/outputs.tf
@@ -0,0 +1,19 @@
+# Flyway Module Outputs
+
+output "ecs_cluster" {
+  description = "ECS cluster information"
+  value = local.create_ecs_cluster ? {
+    id   = aws_ecs_cluster.ecs_cluster[0].id
+    arn  = aws_ecs_cluster.ecs_cluster[0].arn
+    name = aws_ecs_cluster.ecs_cluster[0].name
+  } : null
+}
+
+output "ecs_cluster_id" {
+  description = "ECS cluster ID used by Flyway"
+  value       = local.cluster_id
+}
+output "flyway_task_definition_arn" {
+  description = "ARN of the Flyway task definition"
+  value       = aws_ecs_task_definition.flyway_task_definition[0].arn
+}
